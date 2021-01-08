f4f5ae3dd1b734893f4b5cb5718ced346e196b23
Lucie Fousse <lucie.fousse@grenoble-inp.org>
2021-01-08 10:31:23 +0100

    test token

--- src/test/script/basic-synt.sh
+++ src/test/script/basic-synt.sh
@@ -31,17 +31,34 @@ test_synt_invalide () {
     fi
 }    
 
+test_synt_valide () {
+    # $1 = premier argument.
+    if test_synt "$1" 2>&1 | grep -q -e "$1:[0-9][0-9]*:"
+    then
+        echo "Echec inattendu pour test_synt sur $1."
+        exit 1
+    else
+        echo "Succes attendu de test_synt sur $1."
+    fi
+}   
+
+for cas_de_test in src/test/deca/syntax/invalid/created/*.deca
+do
+    test_synt_invalide "$cas_de_test"
+done
+
 for cas_de_test in src/test/deca/syntax/invalid/provided/*.deca
 do
     test_synt_invalide "$cas_de_test"
 done
 
+for cas_de_test in src/test/deca/syntax/valid/created/*.deca
+do
+    test_synt_valide "$cas_de_test"
+done
 
-if test_synt src/test/deca/syntax/valid/hello.deca 2>&1 | \
-    grep -q -e ':[0-9][0-9]*:'
-then
-    echo "Echec inattendu pour test_synt"
-    exit 1
-else
-    echo "Succes attendu de test_synt"
-fi
+for cas_de_test in src/test/deca/syntax/valid/provided/*.deca
+do
+    test_synt_valide "$cas_de_test"
+done
+
