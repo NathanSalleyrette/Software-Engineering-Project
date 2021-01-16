eae273308640d6e19739a585ec0ccd9f92478299
Benjamin Cathelineau <benjamin.cathelineau@grenoble-inp.org>
2021-01-16 14:42:16 +0100

    Fix tests

--- src/test/script/master-all-test.sh
+++ src/test/script/master-all-test.sh
@@ -6,8 +6,6 @@
 # Lance tout les tests
 # Commentez / retirez du tableau "tests" les tests qui ne passent pas
 tests=("./src/test/script/script_test_general.sh test_lex ./src/test/deca/lexer" "./src/test/script/script_test_general.sh test_synt ./src/test/deca/syntax" "./src/test/script/script_test_general.sh test_context ./src/test/deca/context" "./src/test/script/script_test_general.sh src/test/script/launchers/decac_plus_ima.sh ./src/test/deca/codegen/" "./src/test/script/test_decompile.sh")
-
-tests+=("./src/test/script/launchers/decac_b.sh")
 tests+=("./src/test/script/launchers/decac_r.sh ./src/test/deca/codegen/valid/created/while.deca")
 test_echoue=false
 
