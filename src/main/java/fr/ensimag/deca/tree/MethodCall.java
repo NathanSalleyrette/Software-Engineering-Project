cc1028e8c6aff2ef5482849934447bf1ef21ab7a
Alan Manic <alan.manic@grenoble-inp.org>
2021-01-19 15:18:59 +0100

    modifié :         src/main/java/fr/ensimag/deca/tree/DeclField.java 	modifié :         src/main/java/fr/ensimag/deca/tree/MethodCall.java     - DeclField: l'erreur de la ligne 59 est réindexée en (2.4)     - MethodCall : verify pour l'appel de méthode

--- src/main/java/fr/ensimag/deca/tree/MethodCall.java
+++ src/main/java/fr/ensimag/deca/tree/MethodCall.java
@@ -5,7 +5,9 @@ import java.io.PrintStream;
 import org.apache.commons.lang.Validate;
 
 import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.deca.context.Signature;
 import fr.ensimag.deca.context.ClassDefinition;
+import fr.ensimag.deca.context.MethodDefinition;
 import fr.ensimag.deca.context.ContextualError;
 import fr.ensimag.deca.context.EnvironmentExp;
 import fr.ensimag.deca.context.Type;
@@ -62,7 +64,28 @@ public class MethodCall extends AbstractExpr{
 	@Override
 	public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv, ClassDefinition currentClass)
 			throws ContextualError {
-		// TODO Auto-generated method stub
-		return null;
+		Type objType = this.obj.verifyExpr(compiler, localEnv, currentClass);
+		if (objType == null) throw new ContextualError("(3.71) Le membre gauche n'a pas de type",
+				this.getLocation());
+		if (!objType.isClass()) throw new ContextualError("(3.71) Le membre gauche n'est pas de type 'class'",
+				this.getLocation());
+		ClassDefinition objDef = (ClassDefinition) compiler.getEnvType().get(objType.getName());
+		if (objDef.getMembers().get(this.meth.getName()) == null) {
+			throw new ContextualError("(3.71) " + this.meth.getName().toString() + 
+					" n'est pas une méthode de la classe " + objType.toString(),
+					this.getLocation());
+		}
+		MethodDefinition methodDef = 
+				objDef.getMembers().get(this.meth.getName()).asMethodDefinition(
+					"(3.71) Le champ n'est pas une méthode", this.getLocation());
+		Signature sig = methodDef.getSignature();
+		for (int i = 0; i < sig.size(); i++) {
+			AbstractExpr param = this.params.getList().get(i);
+			param.verifyExpr(compiler, localEnv, currentClass);
+			if (sig.paramNumber(i) != param.getType()) throw new ContextualError(
+					"(3.74) La signature ne correspond pas aux paramètres", this.getLocation());
+		}
+		this.setType(methodDef.getType());
+		return this.getType();
 	}
 }
