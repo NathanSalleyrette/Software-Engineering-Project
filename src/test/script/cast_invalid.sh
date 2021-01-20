6c00e6ab28a30656e3300a7e5d6966cc8ac17bea
Lucie Fousse <lucie.fousse@grenoble-inp.org>
2021-01-20 17:42:01 +0100

    test syntax sur cast + test exhaustif de la 3.39, les test ne sont pas forcement bien classé, je vais bientot reglé ca

---
+++ src/test/script/cast_invalid.sh
@@ -0,0 +1,61 @@
+#! /bin/sh
+
+# Auteur : lucie
+# Version initiale : 12/01/2021
+
+# Generation automatique des tests pour les cast entre type par defaut
+cd "$(dirname "$0")"/../../.. || exit 1
+
+PATH=./src/test/script/launchers:"$PATH"
+chemin="./src/test/deca/context/invalid/created/cast"
+
+
+gen_exp () {
+	args1=$1
+	args2=$2
+	args3=$3
+	filename="$chemin/$cast_${args1}_${args2}.deca"
+	touch  $filename
+	exec 3> $filename
+	echo "// Description :" >&3
+	echo "//    test cast entre un $args1 et $args2" >&3
+	echo "//" >&3
+	echo "// Resultats :" >&3
+	echo "//		Erreur contextuelle" >&3
+	echo "//    	Ligne 10: (3.39) Cast impossible de <type1> vers <type2>
+	" >&3
+	echo "">&3
+	echo "class A {}">&3
+	echo "{" >&3
+	echo "($args1)($args3);" >&3
+	echo "}" >&3
+	exec 3>&-
+}
+
+
+
+
+gen_exp "int" "boolean" "true"
+gen_exp "int" "A" "new A()"
+gen_exp "int" "null" "null"
+gen_exp "float" "int" "1"
+gen_exp "float" "boolean" "true"
+gen_exp "float" "A" "new A()"
+gen_exp "float" "null" "null"
+gen_exp "boolean" "int" "1"
+gen_exp "boolean" "float" "0.5"
+gen_exp "boolean" "A" "new A()"
+gen_exp "boolean" "null" "null"
+gen_exp "void" "int" "1"
+gen_exp "void" "float" "0.5"
+gen_exp "void" "boolean" "true"
+gen_exp "void" "A" "new A()"
+gen_exp "void" "null" "null"
+gen_exp "A" "boolean" "true"
+gen_exp "A" "float" "0.5"
+gen_exp "A" "int" "1"
+gen_exp "A" "null" "null"
+
+
+
+
--- src/test/script/exp_bin_eq_neq_invalid.sh
+++ src/test/script/exp_bin_eq_neq_invalid.sh
@@ -15,6 +15,7 @@ gen_exp_part () {
 	args2=$2
 	args3=$3
 	args4=$4
+	args5=$5
 	filename="$chemin/${args1}_${args2}_${args3}.deca"
 	touch  $filename
 	exec 3> $filename
@@ -24,7 +25,7 @@ gen_exp_part () {
 	echo "// Resultats :" >&3
 	echo "//		Erreur contextuelle" >&3
 	echo "//    	Ligne 11: (3.33) Opérandes de type $args2, $args3, attendu: (int|float, int|float) ou (class|null, class|null) ou (bool, bool)" >&3
-	echo "">&3
+	echo "$args5">&3
 	echo "{" >&3
 	echo "$args2 a;" >&3
 	echo "$args3 b;" >&3
@@ -33,13 +34,22 @@ gen_exp_part () {
 	exec 3>&-
 }
 
+
+
 gen_exp() {
 	nom=$1
 	op=$2
-	gen_exp_part $nom "boolean" "float" "$op"
-	gen_exp_part $nom "boolean" "int" "$op"
-	gen_exp_part $nom "float" "boolean" "$op"
-	gen_exp_part $nom "int" "boolean" "$op"
+	gen_exp_part $nom "boolean" "float" "$op" ""
+	gen_exp_part $nom "A" "float" "$op" "class A{}"
+	gen_exp_part $nom "A" "int" "$op" "class A{}"
+	gen_exp_part $nom "A" "boolean" "$op" "class A{}"
+	gen_exp_part $nom "int" "A" "$op" "class A{}"
+	gen_exp_part $nom "float" "A" "$op" "class A{}"
+	gen_exp_part $nom "boolean" "A" "$op" "class A{}"
+	gen_exp_part $nom "boolean" "int" "$op" ""
+	gen_exp_part $nom "float" "boolean" "$op" ""
+	gen_exp_part $nom "int" "boolean" "$op" ""
+	
 }
 
 
