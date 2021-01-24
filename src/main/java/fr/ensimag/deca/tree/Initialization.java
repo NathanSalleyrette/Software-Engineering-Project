b2dd2f379eefc8959afc4d2b788a38e3dd1fb962
Alan Manic <alan.manic@grenoble-inp.org>
2021-01-24 19:21:45 +0100

    Règle 3.8

--- src/main/java/fr/ensimag/deca/tree/Initialization.java
+++ src/main/java/fr/ensimag/deca/tree/Initialization.java
@@ -39,7 +39,7 @@ public class Initialization extends AbstractInitialization {
     		Type newClass = getExpression().verifyExpr(compiler, localEnv, currentClass);
     		if (!newClass.isSubTypeOf(t, getLocation()))
     			throw new ContextualError(
-    					"(3.9) " + newClass.toString() + " n'est ni la même classe ni une sous-classe de " 
+    					"(3.8) " + newClass.toString() + " n'est ni la même classe ni une sous-classe de " 
     							+ t.toString(), this.getLocation());
     	} else {
     		this.setExpression(getExpression().verifyRValue(compiler, localEnv, currentClass, t));
